e25e2dd80a33448157c57ea35b41a7cd1a264f3e
Dimas Wahyu Nugroho <dimaswahyuuu2005@gmail.com>
2026-08-18 08:31:30 +0700

    membuat component header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,12 +34,8 @@
         <div class="min-h-full">
             <x-navbar />
 
-            <header
-                class="relative bg-gray-800 after:pointer-events-none after:absolute after:inset-x-0 after:inset-y-0 after:border-y after:border-white/10">
-                <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                    <h1 class="text-3xl font-bold tracking-tight text-white">Dashboard</h1>
-                </div>
-            </header>
+            <x-header :tittle="$tittle" />
+
             <main>
                 <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                     <!-- Your content -->
