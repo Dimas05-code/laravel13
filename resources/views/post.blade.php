43edd81bdcfe48d9c663c6cfe717c6169c753bb5
Dimas Wahyu Nugroho <dimaswahyuuu2005@gmail.com>
2026-08-20 20:50:32 +0700

    menambahkan post dan posts

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,12 +2,14 @@
 
     @foreach ($posts as $post)
         <article class="py-8 max-w-screen-md border-b border-gray-300">
-            <h2 class="mb-1 text-3x1 tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
+            <a href="/post/{{ $post['slug'] }}" class="hover:underline">
+                <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['tittle'] }}</h2>
+            </a>
             <div class="text-base tetx-gray-500">
                 <a href="">{{ $post['author'] }}</a>
             </div>
             <p class="my-4 font-light">{{ Str::limit($post['isi'], 100) }}</p>
-            <a href="#" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+            <a href="/post/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
         </article>
     @endforeach
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Termwind\Components\Dd;
+use Illuminate\Support\Arr;
 
+$array = [100, 200, 300];
+
+$first = Arr::first($array, function (int $value, int $key) {
+    return $value >= 150;
+});
+
+// 200
 
 Route::get('/', function () {
     return view('home', ['tittle' => 'home page']);
@@ -13,17 +22,53 @@ Route::get('/contact', function () {
 
 Route::get('/post', function () {
     $posts = [[
-        'title' => 'Article 1',
+        'id' => 1,
+        'slug' => 'article-1',
+        'tittle' => 'Article 1',
         'author' => 'Dimas Wahyu Nugroho',
         'isi' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam quaerat    consectetur alias porro deserunt recusandae error eaque saepe ipsum sapiente dignissimos ut ex quis dolores officia vel maxime, facere reprehenderit.'
     ], [
-        'title' => 'Artikel 2',
+        'id' => 2,
+        'slug' => 'article-2',
+        'tittle' => 'Artikel 2',
         'author' => 'Muhammad Adit',
         'isi' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut eaque expedita enim in
             magnam natus nam repellendus ab laboriosam amet omnis, corrupti consequatur repudiandae ullam consequuntur
             provident temporibus esse assumenda.'
     ]];
     return view('post', ["tittle" => "blog page", 'posts' => $posts]);
+});
+
+// teknik route wildcard => menangkap  nilai dan di masukkean ke variabel
+Route::get('/post/{slug}', function ($slug) {
+    // dd($id);
+
+    // ini hanya simulasi (teknik yang tidak recomended)
+    $posts = [[
+        'id' => 1,
+        'slug' => 'article-1',
+        'tittle' => 'Article 1',
+        'author' => 'Dimas Wahyu Nugroho',
+        'isi' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam quaerat    consectetur alias porro deserunt recusandae error eaque saepe ipsum sapiente dignissimos ut ex quis dolores officia vel maxime, facere reprehenderit.'
+    ], [
+        'id' => 2,
+        'slug' => 'article-2',
+        'tittle' => 'Artikel 2',
+        'author' => 'Muhammad Adit',
+        'isi' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut eaque expedita enim in
+            magnam natus nam repellendus ab laboriosam amet omnis, corrupti consequatur repudiandae ullam consequuntur
+            provident temporibus esse assumenda.'
+    ]];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    // jika id tidak ditemuka
+    if (!$post) abort(404);
+
+    // dd($post);
+    return view('posts', ['tittle' => 'singular post', 'posts' => $post]);
 });
 
 Route::get('/about', function () {
